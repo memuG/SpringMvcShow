3eac18c27a4aa7adfb671bae0f5418705edbfe9f
przemo <p@g.com>
2020-04-17 11:45:41 +0200

    Added Spring Security with optional Config

---
+++ src/main/java/com/przemo/conference/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.przemo.conference.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//@Configuration(proxyBeanMethods = false)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeRequests((req) -> req.anyRequest().hasRole("ADMIN"));
+        http.httpBasic();
+    }
+}
