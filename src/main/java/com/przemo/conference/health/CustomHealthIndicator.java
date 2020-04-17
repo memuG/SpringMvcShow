3e87e3be64d44775fe4cc100f017845997962b0c
przemo <p@g.com>
2020-04-17 16:04:07 +0200

    Added some sensible logic to CustomHealthIndicator, activated detailed view removed WEB-INF

--- src/main/java/com/przemo/conference/health/CustomHealthIndicator.java
+++ src/main/java/com/przemo/conference/health/CustomHealthIndicator.java
@@ -2,13 +2,16 @@ package com.przemo.conference.health;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomHealthIndicator implements HealthIndicator {
     @Override
     public Health health() {
-        return Health.up()
+        boolean invalid = Runtime.getRuntime().maxMemory() < (100*1024*1024);
+        Status status = invalid ? Status.DOWN : Status.UP;
+        return Health.status(status)
                 .withDetail("customValue1", "customValue1 SUCCESS")
                 .withDetail("customValue2", "customValue2 SUCCESS")
                 .build();
